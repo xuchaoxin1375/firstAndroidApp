15b055548b4655b8dd698014dc3b6ea8a77d9bb6
cxxu <838808930@qq.com>
2021-09-14 09:25:16 +0800

    initial push

---
+++ app/src/main/java/com/cxxu/test/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.cxxu.test
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.zjgsu.test.MESSAGE"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    /*Called when the user taps the Secnd button*/
+    fun sendMessage(view: View){
+        /*you can design(implement the function later)*/
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+}
